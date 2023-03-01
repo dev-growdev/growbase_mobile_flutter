203c7986e3b4077a15c7e25a785a76b4d772ab01
Thobias P Silva <thobiaspsilva@yahoo.com.br>
2023-03-01 17:12:04 -0300

    feat: configura o novo tema da aplicação

--- lib/themes/app-colors.theme-extension.dart
+++ lib/themes/app-colors.theme-extension.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 class AppColors extends ThemeExtension<AppColors> {
   final Color? primary;
   final Color? red;
-  final Color? yellow;
-  final Color? blue;
+  final Color? white;
+  final Color? lightBlue;
+  final Color? darkBlue;
   final Color? grey;
   final Color? lightGrey;
   final Color? darkGrey;
@@ -14,8 +15,9 @@ class AppColors extends ThemeExtension<AppColors> {
   const AppColors({
     required this.primary,
     required this.red,
-    required this.yellow,
-    required this.blue,
+    required this.white,
+    required this.lightBlue,
+    required this.darkBlue,
     required this.grey,
     required this.lightGrey,
     required this.darkGrey,
@@ -27,8 +29,9 @@ class AppColors extends ThemeExtension<AppColors> {
   AppColors copyWith({
     Color? primary,
     Color? red,
-    Color? yellow,
-    Color? blue,
+    Color? white,
+    Color? lightBlue,
+    Color? darkBlue,
     Color? grey,
     Color? lightGrey,
     Color? darkGrey,
@@ -38,8 +41,9 @@ class AppColors extends ThemeExtension<AppColors> {
     return AppColors(
       primary: primary ?? this.primary,
       red: red ?? this.red,
-      yellow: yellow ?? this.yellow,
-      blue: blue ?? this.blue,
+      white: white ?? this.white,
+      lightBlue: lightBlue ?? this.lightBlue,
+      darkBlue: darkBlue ?? this.darkBlue,
       grey: grey ?? this.grey,
       lightGrey: lightGrey ?? this.lightGrey,
       darkGrey: darkGrey ?? this.darkGrey,
@@ -56,8 +60,9 @@ class AppColors extends ThemeExtension<AppColors> {
     return AppColors(
       primary: Color.lerp(primary, other.primary, t),
       red: Color.lerp(red, other.red, t),
-      yellow: Color.lerp(yellow, other.yellow, t),
-      blue: Color.lerp(blue, other.blue, t),
+      white: Color.lerp(white, other.white, t),
+      lightBlue: Color.lerp(lightBlue, other.lightBlue, t),
+      darkBlue: Color.lerp(darkBlue, other.darkBlue, t),
       grey: Color.lerp(grey, other.grey, t),
       lightGrey: Color.lerp(lightGrey, other.lightGrey, t),
       darkGrey: Color.lerp(darkGrey, other.darkGrey, t),
--- lib/themes/theme.dart
+++ lib/themes/theme.dart
@@ -1,90 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:growbase_mobile_flutter/themes/app-colors.theme-extension.dart';
 
-const Color _primaryColor = Color(0xFF008065);
+import 'app-colors.theme-extension.dart';
+
+const Color _primaryColor = Color(0xFF004C97);
 
 ThemeData lightTheme() {
   return ThemeData(
-    primaryColor: _primaryColor,
+    useMaterial3: true,
+    brightness: Brightness.light,
+    colorSchemeSeed: _primaryColor,
     fontFamily: 'Inter',
-    appBarTheme: const AppBarTheme(
-      backgroundColor: Colors.white,
-      titleTextStyle: TextStyle(
-        color: Colors.black,
-        fontWeight: FontWeight.bold,
-        fontSize: 20,
-      ),
-      iconTheme: IconThemeData(
-        color: _primaryColor,
-      ),
-    ),
-    scaffoldBackgroundColor: const Color(0xFFF5F5F5),
-    dialogBackgroundColor: Colors.white,
-    cardColor: Colors.white, // Containers and Cards background
-    canvasColor: const Color(0xFFF5F5F5), // Dropdown background
-    textTheme: const TextTheme(
-      /// Topo de página
-      displayLarge: TextStyle(
-        fontWeight: FontWeight.bold,
-        fontSize: 20,
-        color: Colors.black,
-      ),
-
-      /// Título de conteúdo
-      displayMedium: TextStyle(
-        fontWeight: FontWeight.bold,
-        fontSize: 18,
-        color: Colors.black,
-      ),
-
-      /// Descrição de conteúdo
-      bodyMedium: TextStyle(
-        fontWeight: FontWeight.w400,
-        fontSize: 18,
-        color: Colors.black,
-      ),
-
-      bodyLarge: TextStyle(
-        fontWeight: FontWeight.w400,
-        fontSize: 16,
-        color: Colors.black,
-      ),
-
-      labelLarge: TextStyle(
-        fontWeight: FontWeight.bold,
-        fontSize: 18,
-      ),
-
-      // /// Para textos dentro dos inputs
-      titleMedium: TextStyle(
-        fontWeight: FontWeight.normal,
-        fontSize: 18,
-        color: Colors.black,
-      ),
-    ),
+    // scaffoldBackgroundColor: const Color(0xFFF5F5F5),
+    // dialogBackgroundColor: Colors.white,
+    // cardColor: Colors.white, // Containers and Cards background
+    // canvasColor: const Color(0xFFF5F5F5), // Dropdown background
     inputDecorationTheme: const InputDecorationTheme(
-      enabledBorder: OutlineInputBorder(
-          // borderSide: BorderSide(
-          //   color: _primaryColor,
-          // ),
-          ),
-      focusedBorder: OutlineInputBorder(
-        borderSide: BorderSide(
-          color: _primaryColor,
-        ),
-      ),
-      labelStyle: TextStyle(
-        // color: _primaryColor,
-
-        fontWeight: FontWeight.w400,
-        fontSize: 16,
-      ),
+      border: OutlineInputBorder(),
     ),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ButtonStyle(
         shape: MaterialStateProperty.all<RoundedRectangleBorder>(
           RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(100),
+            borderRadius: BorderRadius.circular(6),
+          ),
+        ),
+      ),
+    ),
+    filledButtonTheme: FilledButtonThemeData(
+      style: ButtonStyle(
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(6),
           ),
         ),
       ),
@@ -93,122 +39,64 @@ ThemeData lightTheme() {
       style: ButtonStyle(
         shape: MaterialStateProperty.all<RoundedRectangleBorder>(
           RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(100),
-          ),
-        ),
-        side: MaterialStateProperty.all<BorderSide>(
-          const BorderSide(
-            color: _primaryColor,
-            width: 2,
-            style: BorderStyle.solid,
+            borderRadius: BorderRadius.circular(6),
           ),
         ),
       ),
     ),
-    extensions: [
+    textButtonTheme: TextButtonThemeData(
+      style: ButtonStyle(
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(6),
+          ),
+        ),
+      ),
+    ),
+    extensions: const [
       AppColors(
         primary: _primaryColor,
-        red: const Color(0xFFBD0A0A),
-        yellow: const Color(0xFFCFB848),
-        blue: const Color(0xFF0963BD),
-        grey: const Color(0XFF9EA1A3),
-        lightGrey: const Color(0XFFE8E8E8),
-        darkGrey: Colors.black.withOpacity(0.25),
-        backgroundColor: const Color(0xFFF5F5F5),
-        colorCard: const Color(0XFFFFFFFF),
+        red: Color(0xFFAE0C21),
+        white: Color(0xFFF2F2F2),
+        lightBlue: Color(0xFF57A3ED),
+        darkBlue: Color(0xFF001238),
+        grey: Color(0XFF8B8986),
+        lightGrey: Color(0XFFD7D4D0),
+        darkGrey: Color(0xFF756F6F),
+        backgroundColor: Color(0xFFF5F5F5),
+        colorCard: Color(0XFFFFFFFF),
       ),
     ],
-    colorScheme: ColorScheme.fromSwatch()
-        .copyWith(
-          brightness: Brightness.light,
-          primary: _primaryColor,
-        )
-        .copyWith(background: const Color(0xFFF5F5F5)),
   );
 }
 
 ThemeData darkTheme() {
   return ThemeData(
-    primaryColor: _primaryColor,
+    useMaterial3: true,
+    brightness: Brightness.dark,
+    colorSchemeSeed: _primaryColor,
     fontFamily: 'Inter',
-    appBarTheme: const AppBarTheme(
-      backgroundColor: Color(0XFF2D2825),
-      titleTextStyle: TextStyle(
-        color: Colors.white,
-        fontWeight: FontWeight.bold,
-        fontSize: 20,
-      ),
-      iconTheme: IconThemeData(
-        color: _primaryColor,
-      ),
-    ),
-    scaffoldBackgroundColor: const Color(0XFF2D2825),
-    dialogBackgroundColor: const Color(0XFF463E3A),
-    cardColor: const Color(0XFF463E3A), // Containers and Cards background
-    canvasColor: const Color(0XFF2D2825), // Dropdown background
-    textTheme: const TextTheme(
-      /// Topo de página
-      displayLarge: TextStyle(
-        fontWeight: FontWeight.bold,
-        fontSize: 20,
-        color: Colors.white,
-      ),
-
-      /// Título de conteúdo
-      displayMedium: TextStyle(
-        fontWeight: FontWeight.bold,
-        fontSize: 18,
-        color: Colors.white,
-      ),
-
-      /// Descrição de conteúdo
-      bodyMedium: TextStyle(
-        fontWeight: FontWeight.w400,
-        fontSize: 18,
-        color: Colors.white,
-      ),
-
-      bodyLarge: TextStyle(
-        fontWeight: FontWeight.w400,
-        fontSize: 16,
-        color: Colors.white,
-      ),
-
-      labelLarge: TextStyle(
-        fontWeight: FontWeight.bold,
-        fontSize: 18,
-        color: Colors.white,
-      ),
-
-      // /// Para textos dentro dos inputs
-      titleMedium: TextStyle(
-        fontWeight: FontWeight.normal,
-        fontSize: 18,
-        color: Colors.white,
-      ),
-    ),
+    // scaffoldBackgroundColor: const Color(0XFF2D2825),
+    // dialogBackgroundColor: const Color(0XFF463E3A),
+    // cardColor: const Color(0XFF463E3A), // Containers and Cards background
+    // canvasColor: const Color(0XFF2D2825), // Dropdown background
     inputDecorationTheme: const InputDecorationTheme(
-      enabledBorder: OutlineInputBorder(
-          // borderSide: BorderSide(
-          //   color: _primaryColor,
-          // ),
-          ),
-      focusedBorder: OutlineInputBorder(
-        borderSide: BorderSide(
-          color: _primaryColor,
-        ),
-      ),
-      labelStyle: TextStyle(
-        // color: _primaryColor,
-        fontWeight: FontWeight.w400,
-        fontSize: 16,
-      ),
+      border: OutlineInputBorder(),
     ),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ButtonStyle(
         shape: MaterialStateProperty.all<RoundedRectangleBorder>(
           RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(100),
+            borderRadius: BorderRadius.circular(6),
+          ),
+        ),
+      ),
+    ),
+    filledButtonTheme: FilledButtonThemeData(
+      style: ButtonStyle(
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(6),
           ),
         ),
       ),
@@ -217,36 +105,24 @@ ThemeData darkTheme() {
       style: ButtonStyle(
         shape: MaterialStateProperty.all<RoundedRectangleBorder>(
           RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(100),
-          ),
-        ),
-        side: MaterialStateProperty.all<BorderSide>(
-          const BorderSide(
-            color: _primaryColor,
-            width: 2,
-            style: BorderStyle.solid,
+            borderRadius: BorderRadius.circular(6),
           ),
         ),
       ),
     ),
-    extensions: [
+    extensions: const [
       AppColors(
         primary: _primaryColor,
-        red: const Color(0XFFFF6E6E),
-        yellow: const Color(0xFFCFB848),
-        blue: const Color(0XFF6EB6FF),
-        grey: const Color(0XFF9EA1A3),
-        lightGrey: const Color(0XFFE8E8E8),
-        darkGrey: Colors.black.withOpacity(0.25),
-        backgroundColor: const Color(0XFF2D2825),
-        colorCard: const Color(0XFF463E3A),
+        red: Color(0xFFAE0C21),
+        white: Color(0xFFF2F2F2),
+        lightBlue: Color(0xFF57A3ED),
+        darkBlue: Color(0xFF001238),
+        grey: Color(0XFF8B8986),
+        lightGrey: Color(0XFFD7D4D0),
+        darkGrey: Color(0xFF756F6F),
+        backgroundColor: Color(0XFF2D2825),
+        colorCard: Color(0XFF463E3A),
       ),
     ],
-    colorScheme: ColorScheme.fromSwatch()
-        .copyWith(
-          brightness: Brightness.dark,
-          primary: _primaryColor,
-        )
-        .copyWith(background: const Color(0XFF2D2825)),
   );
 }
