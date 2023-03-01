16dfd4d2c63651947761ea7c4625ac840160378a
Thobias P Silva <thobiaspsilva@yahoo.com.br>
2023-03-01 18:08:11 -0300

    feat: cria a tela splash

---
+++ lib/features/splash/view/splash.page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import '../../../utils/routes.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await Future.delayed(const Duration(seconds: 3));
+      if (!mounted) return;
+      Navigator.of(context).pushReplacementNamed(Routes.login);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          FlutterLogo(
+            size: 200,
+          ),
+          SizedBox(height: 32),
+          CircularProgressIndicator(),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:growbase_mobile_flutter/features/authentication/view/login/login.page.dart';
-import 'package:growbase_mobile_flutter/injection_container.dart' as di;
-import 'package:growbase_mobile_flutter/themes/theme.dart';
-import 'package:growbase_mobile_flutter/utils/routes.dart';
 
 import 'features/authentication/view/create-account/create-account.page.dart';
+import 'features/authentication/view/login/login.page.dart';
 import 'features/categories/view/categories/categories.page.dart';
 import 'features/categories/view/category/category.page.dart';
+import 'features/splash/view/splash.page.dart';
+import 'injection_container.dart' as di;
+import 'shared/view/home/home.page.dart';
+import 'themes/theme.dart';
+import 'utils/routes.dart';
 
 void main() {
   di.init();
@@ -24,77 +26,16 @@ class MyApp extends StatelessWidget {
       theme: lightTheme(),
       darkTheme: darkTheme(),
       routes: {
-        Routes.splashScreen: (_) => const HomeWidget(),
+        Routes.splashScreen: (_) => const SplashPage(),
         Routes.login: (_) => const LoginPage(),
         Routes.createAccount: (_) => const CreateAccountPage(),
+        Routes.home: (_) => const HomePage(),
         Routes.categories: (_) => const CategoriesPage(),
         Routes.category: (ctx) {
           final uid = ModalRoute.of(ctx)?.settings.arguments as String?;
           return CategoryPage(uid: uid);
         },
       },
-    );
-  }
-}
-
-class HomeWidget extends StatelessWidget {
-  const HomeWidget({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              width: 300,
-              child: OutlinedButton(
-                child: const Text('Criar conta'),
-                onPressed: () =>
-                    Navigator.of(context).pushNamed(Routes.createAccount),
-              ),
-            ),
-            SizedBox(
-              width: 300,
-              child: OutlinedButton(
-                child: const Text('Login'),
-                onPressed: () => Navigator.of(context).pushNamed(Routes.login),
-              ),
-            ),
-            SizedBox(
-              width: 300,
-              child: OutlinedButton(
-                child: const Text('Categorias'),
-                onPressed: () =>
-                    Navigator.of(context).pushNamed(Routes.categories),
-              ),
-            ),
-            SizedBox(
-              width: 300,
-              child: OutlinedButton(
-                child: const Text('Nova Categoria'),
-                onPressed: () => Navigator.of(context).pushNamed(
-                  Routes.category,
-                ),
-              ),
-            ),
-            SizedBox(
-              width: 300,
-              child: OutlinedButton(
-                child: const Text('Editar Categoria'),
-                onPressed: () => Navigator.of(context)
-                    .pushNamed(Routes.category, arguments: 'any_uid'),
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
