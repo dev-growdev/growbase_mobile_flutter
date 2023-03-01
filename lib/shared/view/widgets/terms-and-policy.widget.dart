d21077d57e00eb9c1567944fc1eb77063e356a72
Thobias P Silva <thobiaspsilva@yahoo.com.br>
2023-03-01 17:55:59 -0300

    feat: cria componente de termos de uso

---
+++ lib/shared/view/widgets/terms-and-policy.widget.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class TermsAndPolicy extends StatelessWidget {
+  const TermsAndPolicy({super.key});
+
+  void openTerms(BuildContext context) {
+    showBottomSheet(
+      context: context,
+      builder: (context) => Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Termos de Uso',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            const SizedBox(height: 32),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void openPrivacyPolicy(BuildContext context) {
+    showBottomSheet(
+      context: context,
+      builder: (context) => Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Política de Privacidade',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            const SizedBox(height: 32),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: RichText(
+        textAlign: TextAlign.center,
+        text: TextSpan(
+          text: 'Ao clicar em "Entrar", você concorda com os nossos ',
+          style: Theme.of(context).textTheme.bodyMedium,
+          children: [
+            TextSpan(
+              text: 'Termos de Uso',
+              recognizer: TapGestureRecognizer()
+                ..onTap = () => openTerms(context),
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const TextSpan(text: ' e '),
+            TextSpan(
+              text: 'Política de Privacidade.',
+              recognizer: TapGestureRecognizer()
+                ..onTap = () => openPrivacyPolicy(context),
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
