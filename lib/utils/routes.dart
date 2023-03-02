380c896d5cfdbb2d924f93b841fe11ca3cc0edf6
Thobias P Silva <thobiaspsilva@yahoo.com.br>
2023-03-02 15:45:29 -0300

    feat: adiciona rotas do fluxo de criacao de conta

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,6 +1,8 @@
 abstract class Routes {
   static const String splashScreen = '/';
-  static const String createAccount = '/create-account';
+  static const String createAccountEmail = '/account-email';
+  static const String createAccountPersonalData = '/account-personal-data';
+  static const String createAccountPassword = '/account-password';
   static const String login = '/login';
   static const String home = '/home';
   static const String categories = '/categories';
