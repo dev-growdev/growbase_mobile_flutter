5e76c99f96803fa16649c364c4c8ee668858b7aa
Thobias Silva <thobias.silva@growdev.com.br>
2023-07-18 23:15:34 -0300

    refactor: remove o mock

--- lib/features/splash/services/get_user.service.dart
+++ lib/features/splash/services/get_user.service.dart
@@ -1,5 +1,4 @@
 import '../../../shared/adapters/api.adapter.dart';
-import '../../../shared/entities/user.entity.dart';
 import '../../../shared/entities/user_auth.entity.dart';
 
 class GetUserService {
@@ -12,16 +11,4 @@ class GetUserService {
 
     return UserAuth.fromMap(response['data']);
   }
-}
-
-Future<UserAuth> mock() async {
-  await Future.delayed(const Duration(seconds: 2));
-  return const UserAuth(
-    token: 'token',
-    user: User(
-      uid: '123',
-      email: 'thobias@gmail.com',
-      name: 'Thobias S',
-    ),
-  );
 }
